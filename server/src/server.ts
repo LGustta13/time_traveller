130162659d9e010abb27149c1314334a62c0e11a
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2023-05-16 16:59:01 -0300

    Estrutura do backend criada

---
+++ server/src/server.ts
@@ -0,0 +1,20 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+// Cria uma rota
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('✔ HTTP server running on http://localhost:3333')
+  })
