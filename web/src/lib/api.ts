2eb5a0d9dbbadf49da9cbd2460d4ff12d62b1695
Luis Gustavo Oliveira <67337459+LGustta13@users.noreply.github.com>
2023-05-31 19:54:23 -0300

    Update api.ts

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://192.168.100.55:3333', // Antes tava localhost, alterado para o IP do PC pois no server foi colocado app.listen host 0.0.0.0
+  baseURL: 'http://192.168.100.55:3334', // Antes tava localhost, alterado para o IP do PC pois no server foi colocado app.listen host 0.0.0.0
 })
